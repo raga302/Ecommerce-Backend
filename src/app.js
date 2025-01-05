7d6709b06613903707a224a8ece43b6b021c3382
Rahul <149039838+raga302@users.noreply.github.com>
2025-01-05 13:21:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ecommerce-frontend-six-lovat.vercel.app"],
     credentials: true
 }))
 app.use(express.json({ limit: "50mb" }));
